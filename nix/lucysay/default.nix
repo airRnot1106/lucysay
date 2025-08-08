1502384b691a85bd15539476e47aff797d16febc
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-08-08 09:13:17 +0900

    chore: change to compile with bun

--- nix/lucysay/default.nix
+++ nix/lucysay/default.nix
@@ -1,11 +1,44 @@
 {
   buildGleamApplication,
+  stdenv,
+  bun,
 }:
-buildGleamApplication {
+let
   pname = "lucysay";
   version = "0.1.0";
 
-  src = ../..;
+  nix-gleam = buildGleamApplication {
+    inherit pname version;
 
-  target = "javascript";
+    src = ../..;
+
+    target = "javascript";
+  };
+in
+stdenv.mkDerivation {
+  inherit pname version;
+
+  src = nix-gleam.out;
+
+  nativeBuildInputs = [
+    bun
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+    bun build --compile --minify --outfile=lucysay $src/lib/lucysay/main.mjs
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm755 lucysay -t $out/bin
+    runHook postInstall
+  '';
+
+  checkPhase = ''
+    runHook preCheck
+    $out/bin/lucysay --version
+    runHook postCheck
+  '';
 }
